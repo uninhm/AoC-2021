bc217f2659b9f6d51826af36a923db2842d71feb
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-12-02 10:05:34 -0300

    day02

---
+++ day02/part1.d
@@ -0,0 +1,25 @@
+import std.stdio;
+import std.string;
+import std.conv;
+
+void main() {
+    File input = File("input.txt", "r");
+    string line = input.readln.chomp;
+    int distance = 0;
+    int depth = 0;
+    while (!line.empty()) {
+        int space_idx = line.indexOf(' ');
+        string keyword = line[0 .. space_idx];
+        int arg = line[space_idx + 1 .. $].to!int;
+
+        if (keyword == "forward")
+            distance += arg;
+        else if (keyword == "up")
+            depth -= arg;
+        else if (keyword == "down")
+            depth += arg;
+
+        line = input.readln.chomp;
+    }
+    writeln(distance * depth);
+}
---
+++ day02/part2.d
@@ -0,0 +1,28 @@
+import std.stdio;
+import std.string;
+import std.conv;
+
+void main() {
+    File input = File("input.txt", "r");
+    string line = input.readln.chomp;
+    int distance = 0;
+    int depth = 0;
+    int aim = 0;
+    while (!line.empty()) {
+        int space_idx = line.indexOf(' ');
+        string keyword = line[0 .. space_idx];
+        int arg = line[space_idx + 1 .. $].to!int;
+
+        if (keyword == "forward") {
+            distance += arg;
+            depth += aim * arg;
+        }
+        else if (keyword == "up")
+            aim -= arg;
+        else if (keyword == "down")
+            aim += arg;
+
+        line = input.readln.chomp;
+    }
+    writeln(distance * depth);
+}
