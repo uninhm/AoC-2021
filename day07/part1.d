8f2cd8cebcd61ede486ae93f03a4380d79eea89d
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-12-07 11:28:53 -0300

    day07

---
+++ day07/part1.d
@@ -0,0 +1,19 @@
+import std.stdio;
+import std.string;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+
+void main() {
+    File input = File("input.txt", "r");
+    int[] pos = input.readln.chomp.split(",").map!(to!int).array;
+
+    pos.sort;
+    int selected = pos[pos.length/2];
+    
+    int ans = 0;
+    foreach (elem; pos)
+        ans += abs(selected - elem);
+    ans.writeln;
+}
---
+++ day07/part2.d
@@ -0,0 +1,23 @@
+import std.stdio;
+import std.string;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+
+void main() {
+    File input = File("input.txt", "r");
+    int[] pos = input.readln.chomp.split(",").map!(to!int).array;
+
+    int min_ans = int.max;
+    for (int selected = 0; selected < 2000; ++selected) {
+        int ans = 0;
+        foreach (elem; pos) {
+            int d = abs(selected - elem);
+            ans += d*(d+1)/2;
+        }
+        if (ans < min_ans)
+            min_ans = ans;
+    }
+    min_ans.writeln;
+}
