09a01340f563ac5819dd459f384b941ea6a40fb2
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-12-10 01:42:45 -0300

    day06 part1

---
+++ day06/part1.d
@@ -0,0 +1,24 @@
+module day06.part1;
+
+import std.stdio;
+import std.string;
+import std.algorithm;
+import std.conv;
+import std.array;
+
+void main() {
+    File input = File("input.txt", "r");
+    // int[] l = input.readln.chomp.split(",").map!(to!int).array;
+    int[] l = [0];
+    for (int i = 0; i < 80; ++i) {
+        int[] new_generation;
+        foreach (elem; l)
+            if (elem == 0) {
+                new_generation ~= 6;
+                new_generation ~= 8;
+            } else
+                new_generation ~= elem - 1;
+        l = new_generation;
+    }
+    l.length.writeln;
+}
